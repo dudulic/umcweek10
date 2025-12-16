f5339565c436a9ccd156dc0cf3ed9098c879edd2
seungju <lsjxcs@gmail.com>
2025-12-16 23:34:38 +0900

    first commit

---
+++ src/main/java/com/example/umc_week10_mission/UmcWeek10MissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc_week10_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcWeek10MissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcWeek10MissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umc_week10_mission/UmcWeek10MissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_week10_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcWeek10MissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
